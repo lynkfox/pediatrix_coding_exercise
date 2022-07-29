3fb2580128823159dc38f0b4c67fc5783f4ff547
Lynkfox <lynkfox@gmail.com>
2022-07-29 16:46:22 -0400

    Coin tests: Green

--- common/models.py
+++ common/models.py
@@ -5,7 +5,27 @@ from dataclasses import dataclass, field
 class Product:
     """
     A Product for delivery from the Vending Machine.
+
+    Parameters:
+        name: [str] - The name of the product.
+        cost: [float] - The cost of the product in Dollars and Cents (50 cents => .5)
     """
 
     name: str
     cost: float
+
+
+@dataclass
+class Coin:
+    """
+    A Coin object.
+
+    Parameters:
+        name: [str] - The name of the coin.
+        weight: [float] - The weight of the coin, in grams.
+        diameter: [float] - The diameter of the coin, in millimeters
+    """
+
+    name: str
+    weight: float
+    diameter: float
