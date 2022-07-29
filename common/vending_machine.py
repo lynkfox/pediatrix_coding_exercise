fc54eaf3ba7f3c0129af86f9bdde832183542962
Lynkfox <lynkfox@gmail.com>
2022-07-29 19:16:30 -0400

    Documentation update

--- common/vending_machine.py
+++ common/vending_machine.py
@@ -6,6 +6,11 @@ class VendingMachine:
     def __init__(self) -> None:
         """
         A Vending Machine. Comes pre stocked with items.
+
+        Properties:
+            items: [List[Product]] - Products available in the machine.
+            display: [str] - The current message to display
+            current_inserted_value: [float] - the value of inserted coins up to this point.
         """
 
         self.items = [COLA, CANDY, CHIPS]
