a7cb2ee8d2168fa115800c64b0306d36148a3790
Anthony <lynkfox@gmail.com>
2022-07-29 16:31:53 -0400

    Update to Test conformity, Product Tests Green

---
+++ common/models.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Product():
+    """
+    A Product for delivery from the Vending Machine.
+    """
+    name: str
+    cost: float
--- unit_tests/test_models/test_product.py
+++ unit_tests/test_models/test_product.py
@@ -5,13 +5,13 @@ _PRODUCT_COST = 1.50
 
 class Test_Product():
     def setup(self):
-        self._product = Product(Name=_PRODUCT_NAME, Cost=_PRODUCT_COST)
+        self._product = Product(name=_PRODUCT_NAME, cost=_PRODUCT_COST)
 
     def teardown(self):
         del self._product
 
     def test_has_a_name(self):
-        assert self._product.name == _PRODUCT_COST
+        assert self._product.name == _PRODUCT_NAME
 
     def test_name_is_a_string(self):
         assert isinstance(self._product.name, str)
